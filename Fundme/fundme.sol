b2c4c6cefe8250038327a29aca82670f7fddbaa4
Shalu004 <mushaluthakur785@gmail.com>
2025-09-26 19:50:18 +0000

    changes

--- Fundme/fundme.sol
+++ Fundme/fundme.sol
@@ -7,7 +7,7 @@ function withdraw(uint amount) public {
     payable(msg.sender).transfer(amount);
 }
 
-//Wei, Gwei, Ether → denominations of Ether (1 Ether = 10^18 Wei)
+//Wei, Gwei, Ether → denominations of Ether(1 Ether = 10^18 Wei)
 
 //msg.value is a special global variable that holds the amount of Ether (in Wei) sent along with a transaction.
 Creating libraries of or own
