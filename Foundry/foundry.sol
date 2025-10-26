400e3ef3c4fe071256a2cbde99ab8ab1f626a183
sapty_ <mushaluthakur785@gmail.com>
2025-10-26 19:29:19 +0000

    added

--- Foundry/foundry.sol
+++ Foundry/foundry.sol
@@ -1,1 +1,8 @@
 Foundry is a powerful toolchain for Ethereum development, written in Rust, and used mainly for building, testing, and deploying smart contracts.
+
+Foundry is made up of several tools:
+
+>Forge = For building, testing, and deploying smart contracts (similar to Hardhat or Truffle).
+>Cast = For interacting with Ethereum nodes from the command line — sending transactions, reading data, etc.
+>Anvil = A fast local Ethereum node for testing (like Ganache or Hardhat Network).
+>Chisel = A Solidity REPL (lets you test Solidity expressions interactively).
